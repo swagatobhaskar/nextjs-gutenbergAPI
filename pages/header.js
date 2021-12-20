f33f39ae5bf72a161c2d6e88f06c3d307697ca13
Swagato Bhaskar <bhaskarswagato@gmail.com>
2021-12-20 22:15:00 +0530

    added @redux/toolkit redux-saga next-redux-wrapper axios tailwindcss

---
+++ pages/header.js
@@ -0,0 +1,15 @@
+
+export default function Header() {
+    return (
+        <header className="p-20 flex flex-row bg-slate-600">
+            <div className="">
+                <h1 >
+                    The Gutenberg Library
+                </h1>
+                <h3>
+                    About
+                </h3>
+            </div>
+        </header>
+    )
+}
