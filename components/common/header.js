3d0463eb471a2613cd72ec85bb44ce125259c892
Swagato Bhaskar <bhaskarswagato@gmail.com>
2021-12-22 01:22:39 +0530

    CSS changes and added components/container folder for container or smart components.

--- components/common/header.js
+++ components/common/header.js
@@ -2,14 +2,16 @@ import Link from 'next/link';
 
 export default function Header() {
     return (
-        <header className="p-20 flex flex-row bg-slate-400">
-            <div className="">
-                <h1 >
+        <header className="p-5 flex justify-center border-b-2 border-sky-700 bg-gradient-to-r from-cyan-500 to-blue-500">
+            <div className="w-11/12 flex flex-row justify-between">
+                <h1 className='flex items-center font-mono text-5xl font-sm text-header-blue antialiased drop-shadow-3xl'>
                     The Gutenberg Library
                 </h1>
-                <h3>
-                    About
-                </h3>
+                <div className='flex items-center'>
+                    <button className='py-1 px-2 border-2 border-blue-700 hover:bg-blue-600'>
+                        About
+                    </button>
+                </div>
             </div>
         </header>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        '3xl': '4px 5px 2px rgba(0, 0, 0, 0.25)',
+      },
+      colors: {
+        'header-blue': '#235D85',
+      },
+    },
   },
   plugins: [],
 }
