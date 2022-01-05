9fbaade11f818c430ae3e4e57a3ef2a0da379513
Swagato Bhaskar <bhaskarswagato@gmail.com>
2022-01-06 01:26:07 +0530

    next page click changes

--- components/container/bookList.js
+++ components/container/bookList.js
@@ -12,38 +12,65 @@ export default function BookList({bookList}) {
     const newPaginatedBookList = useSelector( state => state.bookList );
     console.log("useSelector: ", newPaginatedBookList);
 
-    const [ renderBookListData, setRenderBookListData ] = useState(newPaginatedBookList || booksToPresent);
+    const [ renderBookListData, setRenderBookListData ] = useState(newPaginatedBookList);
+    const [ nextPageUrl, setNextPageUrl ] = useState('');
+
     const dispatch = useDispatch();
 
-    // useEffect(() => {
-    //     setRenderBookListData(newPaginatedBookList);
-    // }, [newPaginatedBookList])
+    useEffect(() => {
+        setRenderBookListData(newPaginatedBookList);
+    }, [newPaginatedBookList])
 
-    const handleNextPageClick = () => {
+    if (renderBookListData.next !== null){
+        setNextPageUrl(renderBookListData.next);
+    } else {
+        setNextPageUrl(bookList.next);
+    }
+
+    const handleNextPageClick = (nextPageUrl) => {
+        console.log("URL:",nextPageUrl);
         dispatch(fetchPaginatedBookList(nextPageUrl));
     }
 
-    if (newPaginatedBookList) {
+    if (renderBookListData.results) {
         return (
             <Fragment>
                 <div className="">
-                    {newPaginatedBookList.results.map(book => (
+                    {renderBookListData.results.map(book => (
                         <li key={book.id} className='h-52 w-3/4 border-1 border-slate-400 rounded-md mx-auto my-5 drop-shadow-3xl shadow-lg list-none'>
                            <BookListItem book={book} /> 
                         </li>
                     ))}
                 </div>
                 <Pagination
-                    previousPageUrl={newPaginatedBookList.previous}
-                    nextPageUrl={newPaginatedBookList.next}
+                    previousPageUrl={renderBookListData.previous}
+                    nextPageUrl={renderBookListData.next}
                     nextPageClick={handleNextPageClick}
                     // add previousPageClick later
                 />
             </Fragment>
-        )}
-    else {
+        )
+    } else if (bookList) {
         return (
-            <main className='m-auto my-16 w-2/5 border-1 p-5 rounded-sm border-amber-800 shadow-xl shadow-amber-700/20 bg-orange-50 flex flex-col items-center'>
+            <Fragment>
+                <div className="">
+                    {bookList.results.map(book => (
+                        <li key={book.id} className='h-52 w-3/4 border-1 border-slate-400 rounded-md mx-auto my-5 drop-shadow-3xl shadow-lg list-none'>
+                           <BookListItem book={book} /> 
+                        </li>
+                    ))}
+                </div>
+                <Pagination
+                    previousPageUrl={bookList.previous}
+                    nextPageUrl={bookList.next}
+                    nextPageClick={handleNextPageClick}
+                    // add previousPageClick later
+                />
+            </Fragment>
+        )
+    } else {
+        return (
+            <main className='mx-auto my-20 w-3/5 h-min border-1 p-5 rounded-sm border-amber-800 shadow-xl shadow-amber-700/20 bg-orange-50 flex flex-col items-center'>
                 <h2 className='text-amber-600 font-semibold text-2xl'>Oops...</h2>
                 <p className='text-amber-700 font-semibold text-lg'>Something went wrong!</p>
             </main>
--- components/presentation/pagination.js
+++ components/presentation/pagination.js
@@ -1,10 +1,10 @@
-export default function Pagination({previousPageUrl, nextPageUrl, nextPageClick}) {
+export default function Pagination({previousPageUrl, nextPageClick}) {
 
     return (
         <div className='mx-auto mb-3 w-2/5 h-10 flex flex-row'>
             {previousPageUrl ? <a href={previousPageUrl}>Previous</a> : null}
             <button
-                onClick={() => nextPageClick()}
+                onClick={() => nextPageClick(url)}
                 className='m-auto py-1 px-2 rounded-sm bg-blue-600 text-white font-semibold text-sm'
                 >
                 Next Page
