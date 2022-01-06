fc1a62f61cb639dd9099ae34984295b0840e2024
Swagato Bhaskar <bhaskarswagato@gmail.com>
2022-01-06 23:32:34 +0530

    CSS changes

--- components/presentation/authors.js
+++ components/presentation/authors.js
@@ -1,14 +1,15 @@
+import { Fragment } from "react";
 
 const Authors = ({book}) => {
     return (
-        <div className="flex flex-row justify-around">
+        <div className="my-1 flex flex-row justify-around">
             {book.authors.map( author => (
-                <div key={author.name} className=''>
-                    <div className="flex flex-col">
+                <li key={author.name} className=''>
+                    <div className="flex flex-col border-b-2 border-blue-400/80">
                         <p className='font-sans font-light text-sm'>{author.name}</p>
                         <p className='font-sans font-light text-tiny text-center'>{author.birth_year} - {author.death_year}</p>
                     </div>
-                </div>
+                </li>
             ))} 
         </div>
     )
--- components/presentation/bookListItem.js
+++ components/presentation/bookListItem.js
@@ -1,15 +1,15 @@
 import { Fragment } from "react";
 import Image from 'next/image';
+import Link from "next/link";
 
 import Authors from './authors';
-import Link from "next/link";
 
 export default function BookListItem({book}) {
 
     return (
         <Fragment>
             <main className='flex flex-row'>
-                <figure className='ml-4 mt-2 basis-1/4'>
+                <figure className='ml-4 my-2 flex justify-center basis-1/4'>
                     <Image
                         src={book.formats['image/jpeg']}
                         height={190}
@@ -18,7 +18,7 @@ export default function BookListItem({book}) {
                         alt='cover'
                     />
                 </figure>
-                <div className='basis-11/12'>
+                <div className="flex-col m-2 basis-11/12 px-5 py-2">
                     <Link
                         href={{
                             pathname: '/books/[id]',
@@ -26,16 +26,19 @@ export default function BookListItem({book}) {
                         }}
                         passHref
                     >
-                        <a className='px-5 text-left font-sans text-xl font-light'>
+                        <a className='text-left font-sans text-xl font-light'>
                             {book.title}
                         </a>
                     </Link>
                     <Authors book={book} />
-                </div>
-                <div className=''>
-                    <p className=''>
-                        {book.bookshelves}
-                    </p>
+                    <h5 className="text-gray-500 font-normal">Bookshelves:</h5>
+                    <ul className='leading-tight grid grid-rows-4 grid-cols-2'>
+                        {book.bookshelves.map(shelf => (
+                            <li key={shelf} className="pl-2">
+                                <p className="text-sm font-extralight">{shelf}</p>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </main>
         </Fragment>
