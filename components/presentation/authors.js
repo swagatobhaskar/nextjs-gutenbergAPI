a9600c6bbd4d063b4f498177b1b212d2e33eac07
Swagato Bhaskar <bhaskarswagato@gmail.com>
2022-01-07 23:51:58 +0530

    added border-bottom conditional render

--- components/presentation/authors.js
+++ components/presentation/authors.js
@@ -1,14 +1,21 @@
-import { Fragment } from "react";
 
-const Authors = ({book}) => {
+const Authors = ({authorList, borderBottom}) => {
+
     return (
-        <div className="my-1 flex flex-row justify-around">
-            {book.authors.map( author => (
+        <div className="my-1 flex flex-row justify-around list-none">
+            {authorList.map( author => (
                 <li key={author.name} className=''>
-                    <div className="flex flex-col border-b-2 border-blue-400/80">
-                        <p className='font-sans font-light text-sm'>{author.name}</p>
-                        <p className='font-sans font-light text-tiny text-center'>{author.birth_year} - {author.death_year}</p>
-                    </div>
+                    {borderBottom ? (
+                        <div className="flex flex-col border-b-2 border-blue-400/80">
+                            <p className='font-sans font-light text-sm'>{author.name}</p>
+                            <p className='font-sans font-light text-tiny text-center'>{author.birth_year} - {author.death_year}</p>
+                        </div>
+                    ) : (
+                        <div className="flex flex-col">
+                            <p className='font-sans font-light text-sm'>{author.name}</p>
+                            <p className='font-sans font-light text-tiny text-center'>{author.birth_year} - {author.death_year}</p>
+                        </div>
+                    )}
                 </li>
             ))} 
         </div>
